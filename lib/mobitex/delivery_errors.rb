6bfd1794624d46ed658247b3f27f7e0b5cd2c44f
sodercober <sodercober@gmail.com>
2012-02-14 22:15:15 +0000

    Clean up errors modules

--- lib/mobitex/delivery_errors.rb
+++ lib/mobitex/delivery_errors.rb
@@ -1,40 +1,5 @@
 module Mobitex
 
-  class ConnectionError < StandardError
-    attr_reader :response
-
-    def initialize(response, message = nil)
-      @response = response
-      @message  = message
-    end
-
-    def to_s
-      message = "Failed."
-      message << "  Response code = #{response.code}." if response.respond_to?(:code)
-      message << "  Response message = #{response.message}." if response.respond_to?(:message)
-      message
-    end
-  end
-
-  # Raised when a Timeout::Error occurs.
-  class TimeoutError < ConnectionError
-    def initialize(message)
-      @message = message
-    end
-    def to_s; @message ;end
-  end
-
-  # 3xx Redirection
-  class Redirection < ConnectionError # :nodoc:
-    def to_s; response['Location'] ? "#{super} => #{response['Location']}" : super; end
-  end
-
-  # 4xx Client Error
-  class ClientError < ConnectionError; end # :nodoc:
-
-  # 5xx Server Error
-  class ServerError < ConnectionError; end # :nodoc:
-
   # Delivery errors
   class DeliveryError < StandardError
     attr_reader :response
