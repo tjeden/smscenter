f355bdd30254dae3e0d3c1c6b3c7f383566feb50
sodercober <sodercober@gmail.com>
2012-02-15 10:34:45 +0000

    Refactor Errors classes

--- lib/mobitex/connection_errors.rb
+++ lib/mobitex/connection_errors.rb
@@ -14,25 +14,37 @@ module Mobitex
       message << "  Response message = #{response.message}." if response.respond_to?(:message)
       message
     end
+
   end
 
   # Raised when a Timeout::Error occurs.
   class TimeoutError < ConnectionError
+
     def initialize(message)
       @message = message
     end
-    def to_s; @message ;end
+
+    def to_s
+      @message
+    end
+
   end
 
   # 3xx Redirection
   class Redirection < ConnectionError # :nodoc:
-    def to_s; response['Location'] ? "#{super} => #{response['Location']}" : super; end
+
+    def to_s
+      response['Location'] ? "#{super} => #{response['Location']}" : super
+    end
+
   end
 
   # 4xx Client Error
-  class ClientError < ConnectionError; end # :nodoc:
+  class ClientError < ConnectionError
+  end
 
   # 5xx Server Error
-  class ServerError < ConnectionError; end # :nodoc:
+  class ServerError < ConnectionError
+  end
 
 end
