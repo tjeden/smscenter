7ec7ed2f51027b26f7084802d233173ef69375d4
Karol Sarnacki <sodercober@gmail.com>
2012-03-27 09:50:03 +0200

    Add documentation for Status and Response parsers

--- lib/mobitex/response.rb
+++ lib/mobitex/response.rb
@@ -3,6 +3,17 @@ module Mobitex
   class Response
     attr_reader :statuses
 
+    # Public: Parses raw statuses from Mobitex response.
+    #
+    # Examples
+    #
+    #   response = Mobitex::Response.parse("Status: 001, Id: 3e2dc963309c6b574f6c7467a62ef25b, Number: 123456789\nStatus: 106, Id: 251eb8c426466a149bacf15f6c00eacf, Number: 987654321")
+    #   response.statuses.length       # => 2
+    #   response.statuses.first.status # => '001'
+    #   response.statuses.first.id     # => '3e2dc963309c6b574f6c7467a62ef25b'
+    #   response.statuses.first.number # => '123456789'
+    #
+    # Returns Response object.
     def self.parse(raw_response)
       statuses = []
 
--- lib/mobitex/status.rb
+++ lib/mobitex/status.rb
@@ -3,6 +3,16 @@ module Mobitex
   class Status
     attr_reader :attributes
 
+    # Public: Parses single raw status from Mobitex response.
+    #
+    # Examples
+    #
+    #   status = Mobitex::Status.parse('Status: 001, Id: 3e2dc963309c6b574f6c7467a62ef25b, Number: 123456789')
+    #   status.status # => '001'
+    #   status.id     # => '3e2dc963309c6b574f6c7467a62ef25b'
+    #   status.number # => '123456789'
+    #
+    # Returns Status object.
     def self.parse(raw_status)
       attributes = {}
 
